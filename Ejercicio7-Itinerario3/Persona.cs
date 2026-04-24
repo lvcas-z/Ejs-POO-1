6594843693a7b741cac55ae4c945a8114e0f45d8
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2026-04-24 11:51:20 -0300

    add ejs7-it3

---
+++ Ejercicio7-Itinerario3/Persona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7_Itinerario3
+{
+    internal class Persona
+    {
+
+        private string password;
+        private DateTime fechaNacimiento;
+        private string[] telefonos = new string[2];
+
+        private string dato;
+
+        // lectura-escritura
+        public string Nombre { get; set; }
+
+        // auto
+        public string Apellido { get; set; }
+
+        // solo lectura
+        public int Edad
+        {
+            get { return DateTime.Now.Year - fechaNacimiento.Year; }
+        }
+
+        // solo escritura
+        public string Password
+        {
+            set { password = value; }
+        }
+
+        public DateTime FechaNacimiento
+        {
+            set { fechaNacimiento = value; }
+        }
+
+        // propiedad con argumentos
+        public string this[int i]
+        {
+            get { return telefonos[i]; }
+            set { telefonos[i] = value; }
+        }
+
+    }
+}
---
+++ Ejercicio7-Itinerario3/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ejercicio7_Itinerario3;
+
+Console.WriteLine("Hello, World!");
+Persona p = new Persona();
+
+p.Nombre = "Lucas";
+p.Apellido = "Zelaya";
+p.Password = "1234";
+p.FechaNacimiento = new DateTime(2002, 1, 4);
+
+// agrego telefonos
+p[0] = "123456";
+p[1] = "789101";
+
+Console.WriteLine("Nombre: " + p.Nombre);
+Console.WriteLine("Edad: " + p.Edad);
+Console.WriteLine("Teléfono 1: " + p[0]);
+Console.WriteLine("Teléfono 2: " + p[1]);
