b4b44c4c929014f925903d6aede0eb8a77c07b64
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2026-04-24 11:20:19 -0300

    add ejs6-it3

---
+++ Ejercicio6-Itinerario3/Calculadora.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6_Itinerario3
+{
+    internal class Calculadora
+    {
+
+        public int valorInicial { get; set; }
+
+        public Calculadora()
+        {
+            valorInicial = 0;
+        }
+
+        // sobrecarga de constructor
+        public Calculadora(int valor)
+        {
+            valorInicial = valor;
+        }
+
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        // metodo sobrecargado
+        public double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+    
+
+    }
+}
---
+++ Ejercicio6-Itinerario3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ejercicio6_Itinerario3;
+
+Console.WriteLine("Hello, World!");
+
+Calculadora calculadora1 = new Calculadora();
+Calculadora calculadora2 = new Calculadora(10);
+
+Console.WriteLine("Suma int: " + calculadora1.Sumar(2, 3));
+Console.WriteLine("Suma double: " + calculadora1.Sumar(2.5, 3.5));
+
+Console.WriteLine("Valor inicial Calculadora 2: " + calculadora2.valorInicial);
